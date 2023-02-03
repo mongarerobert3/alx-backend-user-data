760311e6a005f739bb54fec05e7835e4a0a48418
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-03 11:29:30 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,17 @@
 import re
 
 
-def filter_datum(fields : str,
-                redaction : str,
-                message : str,
-                separator : str):
-    """ returns the log message obfuscated"""
-    pattern = r"(?i)\b(" + "|".join(fields) + r")\b"
-    return re.sub(pattern, redaction, message)
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
