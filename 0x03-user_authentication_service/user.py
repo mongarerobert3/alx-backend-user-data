da105f06b00aed79b0f6655fe5856be2cbf74e20
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 12:17:15 +0300

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,10 +12,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(128), nullable=False)
-    hashed_password = Column(String(128), nullable=False)
-    session_id = Column(String(128), nullable=True)
-    reset_token = Column(String(128), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __init__(self, email, hashed_password):
         self.email = email
