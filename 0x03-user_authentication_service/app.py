e117be7d844286cdbafe75b335516bb90529b575
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 13:11:48 +0300

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Flask app"""
+
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """ Basic Flask app """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
