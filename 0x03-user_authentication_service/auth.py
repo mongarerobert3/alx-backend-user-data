be59b07d64b17cd4ab1d7669f0385e0b1ee41a44
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 13:09:14 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""hash_password module"""
+""" Authentication
+"""
 from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
@@ -10,8 +11,11 @@ from typing import Union
 
 
 def _hash_password(password: str) -> str:
-    """ Returns a salted, hashed password, which is a byte string. """
+    """ Takes in string arg, converts to unicode
+    Returns salted, hashed pswd as bytestring
+    """
     return hashpw(password.encode('utf-8'), gensalt())
+
 
 def _generate_uuid() -> str:
     """ Generates UUID
@@ -19,17 +23,21 @@ def _generate_uuid() -> str:
     """
     return str(uuid4())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
+        """ Instance """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """ Register user """
+        """ Registers and returns a new user if email isn't listed"""
         try:
             self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
