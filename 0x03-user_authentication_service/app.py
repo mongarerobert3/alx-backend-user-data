9d0a29887ad241850a2e62e47a069321b81632d2
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 13:28:17 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Flask app"""
 
+from typing import Union
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth, _generate_uuid
 from sqlalchemy.orm.exc import NoResultFound
@@ -37,14 +38,6 @@ def valid_login(email: str, password: str) -> bool:
         return False
 
 
-def create_session(email: str) -> str:
-    """ Create a new session """
-    session_id = _generate_uuid()
-    user = AUTH._db.find_user_by(email=email)
-    user.session_id = session_id
-    AUTH._db._session.commit()
-    return session_id
-
 
 @app.route('/login', methods=['POST'], strict_slashes=False)
 def login() -> str:
@@ -57,6 +50,18 @@ def login() -> str:
         response.set_cookie("session_id", session_id)
         return response
     abort(401)
+
+
+def get_user_from_session_id(session_id: str) -> Union[str, None]:
+    """ Get user from session id """
+    if session_id is None:
+        return None
+    try:
+        user = AUTH._db.find_user_by(session_id=session_id)
+        return user
+    except NoResultFound:
+        return None
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
