05f88bf50d54ce9b4510a68c3c1972cb1ab06b6b
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 13:17:12 +0300

    Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,6 +15,17 @@ def welcome() -> str:
     """ Basic Flask app """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ Register a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
