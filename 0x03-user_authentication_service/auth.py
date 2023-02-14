1bd2a599b86825cdc816dba9d847bf370095f77b
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 13:02:56 +0300

    Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""hash_password module"""
+from bcrypt import hashpw, gensalt, checkpw
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from uuid import uuid4
+from typing import Union
+
+
+def _hash_password(password: str) -> str:
+    """ Returns a salted, hashed password, which is a byte string. """
+    return hashpw(password.encode('utf-8'), gensalt())
