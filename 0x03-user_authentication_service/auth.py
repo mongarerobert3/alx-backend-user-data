e43e59a0ad299df3f74e18f1bb381dcead9a5109
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-14 13:05:43 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,3 +12,21 @@ from typing import Union
 def _hash_password(password: str) -> str:
     """ Returns a salted, hashed password, which is a byte string. """
     return hashpw(password.encode('utf-8'), gensalt())
+
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register user """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
